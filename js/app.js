28d7f7f78b19ca762e5330ab91d915439e8a097d
Julien Pouillard <j.pouillard@effigie-creations.com>
2018-03-13 10:26:22 +0100

    Mise en place des couleurs

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@ var app = {
   table: null,
   init: function() {
 
-    console.log('Init');
-
     // On crée le tableau
     app.createTable();
   },
@@ -18,7 +16,7 @@ var app = {
     for (var line = 0; line < 8; line++) {
 
       // On crée une ligne du tableau
-      app.createLine();
+      app.createLine( line );
     }
 
     // On affiche le <table> dans la <div> "chess"
@@ -28,16 +26,26 @@ var app = {
     div.appendChild( app.table );
   },
 
-  createLine: function() {
+  createLine: function( lineIndex ) {
 
     // On utilise des boucles pour dessiner
     // les lignes et les colonnes du tableau
     var tr = document.createElement('tr');
 
+    // On crée un interrupteur
+    var interrupteur = (lineIndex % 2 === 0);
+
     for (var column = 0; column < 8; column++) {
 
       // On crée un <td>
       var td = document.createElement('td');
+
+      // On applique une couleur de fond
+      if (interrupteur) td.style.backgroundColor = '#efe0d1';
+      else td.style.backgroundColor = '#70433c';
+
+      // On change la valeur de l'interrupteur
+      interrupteur = !interrupteur;
 
       // On ajoute le <td> dans le <tr>
       tr.appendChild( td );
